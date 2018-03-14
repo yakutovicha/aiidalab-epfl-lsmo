3309379d6c1253503566e67f0c942c35e71a3b63
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2018-03-14 19:56:38 +0100

    Initial commit

---
+++ start.py
@@ -0,0 +1,31 @@
+import ipywidgets as ipw
+
+def get_start_widget(appbase, jupbase):
+    #http://fontawesome.io/icons/
+    template = """
+    <table>
+    <tr>
+        <th style="text-align:center">Calculations</th>
+        <th style="width:70px" rowspan=2></th>
+        <th style="text-align:center">Results analysis</th>        
+        <th style="width:70px" rowspan=2></th>
+        <th style="text-align:center">Import</th>        
+    <tr>
+    <td valign="top"><ul>
+    <li><a href="{appbase}/calculations/isotherm.ipynb" target="_blank">Isotherm</a>
+    </ul></td>
+    
+    <td valign="top"><ul>
+    <li><a href="{appbase}/results/analysis.ipynb" target="_blank">Analysis</a>
+    </ul></td>
+    
+     <td valign="top"><ul>
+    <li><a href="{appbase}/import/import.ipynb" target="_blank">Import database</a>
+    </ul></td>
+    </tr></table>
+"""
+    
+    html = template.format(appbase=appbase, jupbase=jupbase)
+    return ipw.HTML(html)
+    
+#EOF
