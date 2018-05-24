ad540fc079374fd5f94da46983cc5bc3e82d8e5b
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2018-05-24 16:45:38 +0000

    Reorganizing the app

--- start.py
+++ start.py
@@ -5,26 +5,27 @@ def get_start_widget(appbase, jupbase):
     template = """
     <table>
     <tr>
-        <th style="text-align:center">Calculations</th>
+        <th style="text-align:center">Isotherm</th>
         <th style="width:70px" rowspan=2></th>
-        <th style="text-align:center">Results analysis</th>        
+        <th style="text-align:center">Charges</th>        
         <th style="width:70px" rowspan=2></th>
-        <th style="text-align:center">Import</th>        
+        <th style="text-align:center">Import your data to AiiDA</th>        
     <tr>
     <td valign="top"><ul>
-    <li><a href="{appbase}/calculations/isotherm.ipynb" target="_blank">Isotherm</a>
-    <li><a href="{appbase}/calculations/isotherm_multi.ipynb" target="_blank">Isotherm (multi)</a>
-    <li><a href="{appbase}/results/database_analysis.ipynb" target="_blank">Isotherm analysis</a>
+    <li><a href="{appbase}/isotherm/isotherm.ipynb" target="_blank">Compute one</a>
+    <li><a href="{appbase}/isotherm/isotherm_multi.ipynb" target="_blank">Compute multiple</a>
+    <li><a href="{appbase}/isotherm/database_analysis.ipynb" target="_blank">Analysis</a>
+    </ul></td>
+     
+     <td valign="top"><ul>
+    <li><a href="{appbase}/charges/compute_charges.ipynb" target="_blank">Compute Charges</a>
     </ul></td>
     
     <td valign="top"><ul>
-    <li><a href="{appbase}/results/analysis.ipynb" target="_blank">Preprocess</a>
-    <li><a href="{appbase}/results/plot.ipynb" target="_blank">Plot</a>
+    <li><a href="{appbase}/import/import.ipynb" target="_blank">Import database</a>
+    <li><a href="{appbase}/import/plot.ipynb" target="_blank">Plot imported data</a>
     </ul></td>
     
-     <td valign="top"><ul>
-    <li><a href="{appbase}/import/import.ipynb" target="_blank">Import database</a>
-    </ul></td>
     </tr></table>
 """
     
