40e05957e549f6d1931c8949173573a94f893915
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2019-10-29 14:45:43 +0000

    Add multistage workchain submission app.

--- start.py
+++ start.py
@@ -8,6 +8,8 @@ def get_start_widget(appbase, jupbase):
         <th style="text-align:center">Isotherm</th>
         <th style="width:70px" rowspan=2></th>
         <th style="text-align:center">Pore analysis</th>        
+        <th style="width:70px" rowspan=2></th>
+        <th style="text-align:center">Geometry Optimization</th>
     <tr>
     <td valign="top"><ul>
     <li><a href="{appbase}/isotherm/isotherm.ipynb" target="_blank">Compute one</a>
@@ -18,6 +20,10 @@ def get_start_widget(appbase, jupbase):
     <li><a href="{appbase}/pores/pores.ipynb" target="_blank">Compute Pores</a>
     </ul></td>
     
+    <td valign="top"><ul>
+    <li><a href="{appbase}/multistage_geo_opt.ipynb" target="_blank">Multistage Geo Opt</a>
+    <li><a href="{appbase}/isotherm/isotherm_multi.ipynb" target="_blank">Analyse results</a>
+    </ul></td>
     </tr></table>
 """
     
