16210087811010131c321f8f01f5d3483fdbaa6d
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-03-06 07:46:06 +0000

    Remove charges app.
    The app was basically empty and there is currently no plans
    to support it.

--- start.py
+++ start.py
@@ -7,8 +7,6 @@ def get_start_widget(appbase, jupbase):
     <tr>
         <th style="text-align:center">Isotherm</th>
         <th style="width:70px" rowspan=2></th>
-        <th style="text-align:center">Charges</th>        
-        <th style="width:70px" rowspan=2></th>
         <th style="text-align:center">Pore analysis</th>        
     <tr>
     <td valign="top"><ul>
@@ -17,10 +15,6 @@ def get_start_widget(appbase, jupbase):
     <li><a href="{appbase}/isotherm/database_analysis.ipynb" target="_blank">Analysis</a>
     </ul></td>
      
-     <td valign="top"><ul>
-    <li><a href="{appbase}/charges/compute_charges.ipynb" target="_blank">Compute Charges</a>
-    </ul></td>
-
      <td valign="top"><ul>
     <li><a href="{appbase}/pores/pores.ipynb" target="_blank">Compute Pores</a>
     <li><a href="{appbase}/barcode.ipynb" target="_blank">Compute Barcode</a></li>
