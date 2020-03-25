119349506cbe04940ced0a926d134d799bbcc575
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-03-25 07:58:16 +0100

    Add a notebook to run Multistage and DDEC calculations. (#11)

--- start.py
+++ start.py
@@ -22,6 +22,7 @@ def get_start_widget(appbase, jupbase):
     
     <td valign="top"><ul>
     <li><a href="{appbase}/multistage_geo_opt.ipynb" target="_blank">Geometry Optimization</a>
+    <li><a href="{appbase}/multistage_geo_opt_ddec.ipynb" target="_blank">Geometry Optimization and Charges</a>
     </ul></td>
     </tr></table>
 """
