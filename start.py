8944b31bc5ac46f58c610654127c1e18e4ea69fd
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-09-03 23:47:05 +0000

    Reorganize the notebook links on the start page
    + remove unnecessary files

--- start.py
+++ start.py
@@ -5,29 +5,34 @@ def get_start_widget(appbase, jupbase):
     template = """
     <table>
     <tr>
-        <th style="text-align:center">Isotherm</th>
+        <th style="text-align:center">Prepare the structure</th>
         <th style="width:70px" rowspan=2></th>
         <th style="text-align:center">Pore analysis</th>        
         <th style="width:70px" rowspan=2></th>
-        <th style="text-align:center">Geometry Optimization</th>
+        <th style="text-align:center">Isotherm calculations</th>
     <tr>
-    <td valign="top"><ul>
-    <li><a href="{appbase}/isotherm/isotherm.ipynb" target="_blank">Compute one</a>
-    <li><a href="{appbase}/isotherm/henry_coefficient.ipynb" target="_blank">Compute Henry Coefficient</a>
-    <li><a href="{appbase}/isotherm/analyse_results.ipynb" target="_blank">Analyse the results</a>
-    <li><a href="{appbase}/results.ipynb?process_label=IsothermWorkChain" target="_blank">Results</a>
-    </ul></td>
-     
-     <td valign="top"><ul>
-    <li><a href="{appbase}/pores/pore_analysis.ipynb" target="_blank">Pore Analysis</a>
-    <li><a href="{appbase}/results.ipynb?process_label=NetworkCalculation" target="_blank">Results</a>
-    </ul></td>
-    
+
     <td valign="top"><ul>
     <li><a href="{appbase}/multistage_geo_opt.ipynb" target="_blank">Geometry Optimization</a>
     <li><a href="{appbase}/multistage_geo_opt_ddec.ipynb" target="_blank">Geometry Optimization and Charges</a>
     <li><a href="{appbase}/results.ipynb?process_label=Cp2kMultistageDdecWorkChain" target="_blank">Results</a>
     </ul></td>
+
+    <td valign="top"><ul>
+    <li><a href="{appbase}/pore_analysis.ipynb" target="_blank">Pore Analysis</a>
+    <li><a href="{appbase}/results.ipynb?process_label=NetworkCalculation" target="_blank">Results</a>
+    </ul></td>
+
+    <td valign="top"><ul>
+    <li><a href="{appbase}/compute_isotherm.ipynb" target="_blank">Compute one</a>
+    <li><a href="{appbase}/compute_henry_coefficient.ipynb" target="_blank">Compute Henry Coefficient</a>
+    <li><a href="{appbase}/analyse_isotherm_results.ipynb" target="_blank">Analyse the results</a>
+    <li><a href="{appbase}/results.ipynb?process_label=IsothermWorkChain" target="_blank">Results</a>
+    </ul></td>
+     
+
+    
+
     </tr></table>
 """
     
